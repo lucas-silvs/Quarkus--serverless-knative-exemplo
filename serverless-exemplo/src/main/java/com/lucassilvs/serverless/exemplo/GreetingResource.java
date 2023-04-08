f231b7734f82cecd8699367e6656adc6ddd4497e
Lucas Santos <lucas.parkl@outlook.com>
2023-04-08 15:24:03 -0300

    Quarkus Serverless com Funqy: - envio do projeto inicial seguindo documentação - alterada porta de execução para 5000

---
+++ serverless-exemplo/src/main/java/com/lucassilvs/serverless/exemplo/GreetingResource.java
@@ -0,0 +1,22 @@
+package com.lucassilvs.serverless.exemplo;
+
+import io.quarkus.funqy.Funq;
+
+public class GreetingResource {
+
+    @Funq
+    public String hello() {
+        return "Hello World";
+    }
+
+    @Funq
+    public String toLowerCase(String val) {
+        return val.toLowerCase();
+    }
+
+    //Alterando nome endpoint de acesso para execução da função
+    @Funq("double")
+    public int doubleIt(int val) {
+        return val + val;
+    }
+}
